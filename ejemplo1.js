e1e23d1626d0592ebb25863c97007b520de50193
Cristian Vanegas <cristian.vanegas@newshore.es>
2023-08-14 14:51:15 -0500

    primer ejemplo con ts

---
+++ ejemplo1.js
@@ -0,0 +1,4 @@
+function calcularEnvio(precioProducto, valorEncomienda) {
+    console.log(precioProducto + valorEncomienda);
+}
+calcularEnvio(200000, '20000');
---
+++ ejemplo1.ts
@@ -0,0 +1,5 @@
+function CalcularEnvio(precioProducto:number, valorEncomienda:number):number{
+    //console.log(precioProducto+valorEncomienda); 
+    return precioProducto+valorEncomienda;   
+}
+CalcularEnvio(200000, 20000);
